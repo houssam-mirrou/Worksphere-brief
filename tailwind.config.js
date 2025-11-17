9c1f795f7e6dfc48820f78dc58aa0a11ea1bebc6
houssam-mirrou <hossam.mirrou@gmail.com>
2025-11-17 13:15:37 +0100

    feat/layout-structure structure de layout en html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./docs/**/*.{html,js}", 
+  content: ["./docs/**/*.{html,js}",
     "./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage : {
+        plan_phone:  "url('/docs/img/phone.png')",
+        plan_desktop:  "url('/docs/img/desktop.png')"
+      }
+    },
   },
   plugins: [],
 }
